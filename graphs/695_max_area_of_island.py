c4ccf9177454aa7224ba122da2bf049a74cbe9fb
nandakishore <nk732100@gmail.com>
2025-05-07 23:36:35 +0530

    Create 695_max_area_of_island.py

---
+++ graphs/695_max_area_of_island.py
@@ -0,0 +1,36 @@
+from typing import List 
+
+
+def max_area_of_island(grid: List[List[int]]) -> int: 
+    ROWS, COLS = len(grid), len(grid[0])
+    visited = set()
+    max_area = 0
+
+    def dfs(x, y, area=0):
+        nonlocal ROWS, COLS, visited
+        
+        if (x, y) not in visited and x >= 0 and x < ROWS and y >= 0 and y < COLS and grid[x][y] == 1:
+        
+            visited.add((x, y))
+            area += 1
+
+            area = dfs(x + 1, y, area)
+            area = dfs(x - 1, y, area)
+            area = dfs(x, y + 1, area)
+            area = dfs(x, y - 1, area)
+    
+        return area
+    
+    for row in range(ROWS):
+        for col in range(COLS):
+            if grid[row][col] == 1:
+                area = dfs(row, col)
+                max_area = area if area > max_area else max_area
+    
+    return max_area
+
+
+if __name__ == "__main__":
+    print("main called")
+    grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    print(max_area_of_island(grid))
